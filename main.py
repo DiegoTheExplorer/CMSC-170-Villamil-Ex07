bf45f94536255e9ef586c71bf867ebe70ab83f72
Diego Villamil <dmvillamil@up.edu.ph>
2023-11-16 02:54:37 +0800

    almost all feat vectors match expected category

--- main.py
+++ main.py
@@ -1,15 +1,83 @@
 from decimal import *
+from math import sqrt
 
-diabetesFp = open("diabetes.csv", "r")
+def sort_key(nghbr):
+  return nghbr[1]
+
+kn = 4
+kNearest = []
 diabetes = []
+input = []
+distList = []
+output = []
 
-for line in diabetesFp:
+fp = open("diabetes.csv", "r")
+for line in fp: #Read the data from diabetes .csv
   temp = line.split(",")
   temp = list(map(Decimal,temp))
   diabetes.append(temp)
+fp.close()
 
-for row in diabetes:
+#prints the data in diabetes
+# for row in diabetes:
+#   for val in row:
+#     print(val, " ", end="")
+#   print("\n")
+
+fp = open("input.in", "r")
+for line in fp: #Read the data from input.in
+  temp = line.split(",")
+  temp = list(map(Decimal,temp))
+  input.append(temp)
+
+#prints the data in input
+# for row in input:
+#   for val in row:
+#     print(val, " ", end="")
+#   print("\n")
+
+dlen = len(diabetes)
+inplen = len(input)
+
+for i in range(0,inplen): #Loop through each feature vector in input
+  distList.clear()
+  kNearest.clear()
+  kn = 4
+  diabetic = 0
+  nondiabetic = 0
+
+  for j in range(0,dlen): #Loop through each feature vector in diabetes
+    dist = 0
+    for k in range(0,(len(input[0]) - 1)): # Calculate the distance of feature vector input[i] to diabetes[j]
+      dist += sqrt(abs(input[i][k] - diabetes[j][k]) ** 2)
+    distList.append((j,dist))
+  
+  distList.sort(key = sort_key)
+
+  for k in range(0,k):
+    if (diabetes[distList[k][0]][8] == 0):
+      nondiabetic += 1
+    else:
+      diabetic += 1
+
+  while(diabetic == nondiabetic): #Breaking ties by looking at the next nearest neighbor
+    if (diabetes[kn][8] == 0):
+      nondiabetic += 1
+    else:
+      diabetic += 1
+    kn += 1
+
+  if(diabetic > nondiabetic):
+    temp = input[i].copy()
+    temp.append(1)
+  elif(nondiabetic > diabetic):
+    temp = input[i].copy()
+    temp.append(0)
+  
+  output.append(temp)
+  diabetes.append(output[i])
+  
+for row in output:
   for val in row:
-    print(val, " ", end="")
+    print(val," ", end="")
   print("\n")
-diabetesFp.close()
